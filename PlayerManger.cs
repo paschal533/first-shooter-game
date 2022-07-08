d1c5d0d9d1dd048782c146b29500e5a7969683a0
paschal533 <okwuosahpaschal@gmail.com>
2022-07-08 14:08:04 -0700

    PlayerManager commit

---
+++ PlayerManger.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerManager : MonoBehaviour
+{
+    public float health = 100;
+
+    public void Hit(float damage)
+    {
+        health -= damage;
+
+        if(health <= 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
