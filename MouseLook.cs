22391aacae61880e3b8409b58ae2f1b9326960fb
paschal533 <okwuosahpaschal@gmail.com>
2022-07-07 22:42:58 -0700

    first commit

--- MouseLook.cs
+++ MouseLook.cs
@@ -22,7 +22,7 @@ public class MouseLook : MonoBehaviour
          float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
          xRotation -= mouseY;
          xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-         //transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
          playerBody.Rotate(Vector3.up * mouseX);
     }
 }
