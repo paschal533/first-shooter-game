44ce1945dba2c5d9141cdea5894ed9af7158f6d0
paschal533 <okwuosahpaschal@gmail.com>
2022-07-08 14:11:30 -0700

    WeaponManager commit

---
+++ EnemieManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemieManager : MonoBehaviour
+{
+    public GameObject player;
+    public Animator enemyAnimator;
+    public float damage = 20f;
+    public float enemyHealth = 100f;
+
+    public void Hit(float damage)
+    {
+        enemyHealth -= damage;
+        if(enemyHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<NavMeshAgent>().destination = player.transform.position;
+        if(GetComponent<NavMeshAgent>().velocity.magnitude > 1)
+        {
+            enemyAnimator.SetBool("isRuning", true);
+        }
+        else
+        {
+            enemyAnimator.SetBool("isRuning", false);
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject == player)
+        {
+            player.GetComponent<PlayerManager>().Hit(damage);
+        }
+    }
+}
